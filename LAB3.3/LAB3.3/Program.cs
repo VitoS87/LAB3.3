9c94e813e0c173e0b42c9d897c1926941e73a005
Vitos <rustamminis@gmail.com>
2025-10-26 22:52:05 +0200

    Add files via upload

---
+++ LAB3.3/LAB3.3/Program.cs
@@ -0,0 +1,27 @@
+﻿try
+{
+    Console.Write("Введите n:");
+    long n = long.Parse(Console.ReadLine());
+    Console.Write("Введите x:");
+    double x = double.Parse(Console.ReadLine());
+    double s = 0;
+    double y = 1;
+    double z;
+    double f = 1;
+    for (long i = 0; i < n; i++)
+    {
+        for (int j = 1; j <= 1+i*2; j++)
+        {
+            f *= j;
+        }
+        z = Math.Pow(x, 1 + i * 2);
+        s += y * (z / f);
+        y *= -1;
+        f = 1;
+    }
+    Console.WriteLine("Сумма= " + s);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
